f4469fff95c4249b3c2574020e94dcd3318cda5a
habib <habib@in-school.net>
2022-07-08 21:39:46 +0600

    POS Product & Rental  in progress

--- src/test/Features/FrstTest.feature
+++ src/test/Features/FrstTest.feature
@@ -3,25 +3,40 @@ Feature: Class
 #  Scenario: Login
 #    Given I go to this url "https://habibmaternitycenter.momscradle.com/admin/login"
 #    When I Login using user name "habib@in-school.net" and password "12345678"
+#
+#  Scenario: Class type CRUD
+#    Given I go to this url "https://habibmaternitycenter.momscradle.com/admin/login"
+#    When I Login using user name "habib@in-school.net" and password "12345678"
+#    Then I go to class type
+#    Then I add class type
+#    Then I edit class type
+#    Then I remove class type
 
-  Scenario: Class type CRUD
-    Given I go to this url "https://habibmaternitycenter.momscradle.com/admin/login"
-    When I Login using user name "habib@in-school.net" and password "12345678"
-    Then I go to class type
-    Then I add class type
-    Then I edit class type
-    Then I remove class type
-
-  Scenario: Class CRUD
-    Given I go to this url "https://habibmaternitycenter.momscradle.com/admin/login"
-    When I Login using user name "habib@in-school.net" and password "12345678"
-    Then I go to class
-    Then I add class
-    Then I edit class
-    Then I remove class
+#  Scenario: Class CRUD
+#    Given I go to this url "https://habibmaternitycenter.momscradle.com/admin/login"
+#    When I Login using user name "habib@in-school.net" and password "12345678"
+#    Then I go to class
+#    Then I add class
+#    Then I edit class
+#    Then I remove class
 
   Scenario: POS Class
     Given I go to this url "https://habibmaternitycenter.momscradle.com/admin/login"
     When I Login using user name "habib@in-school.net" and password "12345678"
     Then I go to POS
-    Then Select & Pay for class
+    Then I Select class
+    Then I make payment
+
+  Scenario: POS Product
+    Given I go to this url "https://habibmaternitycenter.momscradle.com/admin/login"
+    When I Login using user name "habib@in-school.net" and password "12345678"
+    Then I go to POS
+    Then I Select Product
+    Then I make payment
+
+  Scenario: POS Rental
+    Given I go to this url "https://habibmaternitycenter.momscradle.com/admin/login"
+    When I Login using user name "habib@in-school.net" and password "12345678"
+    Then I go to POS
+    Then I Select Rental
+    Then I make payment
--- src/test/java/com/stepdefinations/firstClass.java
+++ src/test/java/com/stepdefinations/firstClass.java
@@ -31,23 +31,25 @@ public class firstClass extends World {
         world.driver = new ChromeDriver();
 
     }
+
     @AfterStep
-    public void afterStep(){
+    public void afterStep() {
         new WebDriverWait(world.driver, Duration.ofSeconds(world.timeOut)).until(
                 webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
     }
-
-    @After
-    public void after() {
-        if (world.driver != null) {
-            world.driver.close();
-        }
-    }
+//
+//    @After
+//    public void after() {
+//        if (world.driver != null) {
+//            world.driver.close();
+//        }
+//    }
 
     @Given("I go to this url {string}")
     public void i_go_to_this_url(String url) {
         world.driver.get(url);
     }
+
     @When("I Login using user name {string} and password {string}")
     public void i_login_using_user_name_and_password(String userName, String password) {
         world.driver.manage().window().maximize();
@@ -68,6 +70,7 @@ public class firstClass extends World {
         world.driverWait("//div[@class=\"submenu\"]//descendant::a[contains(text(), 'Manage Class Type')]").click();
 
     }
+
     @Then("I add class type")
     public void i_add_class_type() {
         world.driverWait("//input[@name='type name']").sendKeys(new CharSequence[]{"Automation Test CRUD"});
@@ -77,6 +80,7 @@ public class firstClass extends World {
         assert classType_ADD.contains("success");
 
     }
+
     @Then("I edit class type")
     public void i_edit_class_type() {
         world.driverWait("//td[contains(text(), 'CRUD')]//following-sibling::td//a[contains(text(), 'Edit')]").click();
@@ -89,6 +93,7 @@ public class firstClass extends World {
         assert classType_EDIT.contains("success");
 
     }
+
     @Then("I remove class type")
     public void i_remove_class_type() {
         world.driverWait("//td[contains(text(), 'CRUD')]//following-sibling::td//a[contains(text(), 'Remove')]").click();
@@ -98,6 +103,7 @@ public class firstClass extends World {
         assert classType_REMOVE.contains("success");
 
     }
+
     @Then("I go to class")
     public void i_go_to_class() {
         world.driverWait("//span[contains(text(),\"Shopping Services\")]").click();
@@ -106,13 +112,13 @@ public class firstClass extends World {
         world.driverWait("(//div[@class=\"submenu\"]//descendant::a[contains(text(), 'Add Class')])[1]").click();
 
     }
+
     @Then("I add class")
     public void i_add_class() {
         world.driverWait("//input[@name='title']").sendKeys("Automation Test CRUD");
-        WebElement dropdown = world.driverWait("//label[text()='Class Type']//parent::div//select");
-        Select classType = new Select(dropdown);
-        System.out.println(classType);
-        classType.selectByIndex(1);
+        WebElement select = world.driverWait("//label[text()='Class Type']//parent::div//select");
+        Select selectClassType = new Select(select);
+        selectClassType.selectByIndex(1);
 
         world.driverWait("//input[@name='year']").sendKeys("2022");
         world.driverWait("//input[@name='fee']").sendKeys("200");
@@ -123,6 +129,7 @@ public class firstClass extends World {
         String CLASS_ADD = world.driverWait("//div[@class='toast-message']").getText();
         assert CLASS_ADD.contains("success");
     }
+
     @Then("I edit class")
     public void i_edit_class() {
 //        world.driverWait("//input[@placeholder=\"Search Classes\"]").sendKeys("CRUD");
@@ -141,6 +148,7 @@ public class firstClass extends World {
         String CLASS_EDIT = world.driverWait("//div[@class='toast-message']").getText();
         assert CLASS_EDIT.contains("success");
     }
+
     @Then("I remove class")
     public void i_remove_class() {
 //        world.driverWait("//input[@placeholder=\"Search Classes\"]").sendKeys("CRUD");
@@ -162,11 +170,12 @@ public class firstClass extends World {
     public void i_go_to_pos() {
         world.driverWait("(//div[@data-v-step=\"3\"]//li)[1]").click();
         world.driverWait("//button[contains(text(), 'Walk-in Client')]").click();
-        //world.driverWait("//input[@placeholder=\"Search User\"]").sendKeys("user");
-
+//        world.driverWait("//input[@role=\"searchbox\"]").sendKeys("user23");
+//        world.driverWait("//small[@class=\"main-title\"]").click();
     }
-    @Then("Select & Pay for class")
-    public void select_pay_for_class() {
+
+    @Then("I Select class")
+    public void i_select_class() {
 
         WebElement dropdown = world.driverWait("//select[@class=\"form-control\"]");
         Select POSclass = new Select(dropdown);
@@ -179,7 +188,10 @@ public class firstClass extends World {
 
         String Added_to_Cart = world.driverWait("//div[@class='toast-message']").getText();
         assert Added_to_Cart.contains("success");
+    }
 
+    @Then("I make payment")
+    public void i_make_payment() {
 
 //        JavascriptExecutor js = (JavascriptExecutor) world.driver;
 //        js.executeScript("window.scrollBy(0,-1900)");
@@ -189,16 +201,119 @@ public class firstClass extends World {
         world.driverWait("//button[contains(text(), 'Provide Payment Info')]").click();
         world.driverWait("//button[contains(text(), ' Order Preview ')]").click();
         world.driverWait("//button[contains(text(), ' Make Payment ')]").click();
-        world.driverWait("//input[@id=\"cardNumber\"]").sendKeys("4111111111111111");
-        world.driverWait("//input[@id=\"cardExpiry\"]").sendKeys("09/30");
-        world.driverWait("//input[@id=\"cardCvc\"]").sendKeys("123");
-        world.driverWait("//input[@id=\"billingName\"]").sendKeys("Md Habibullah Mia");
-        world.driverWait("//button[@type=\"submit\"]").click();
 
-//        String POS_Class = world.driverWait("//div[@class='toast-message']").getText();
-//        assert POS_Class.contains("success");
 
+        if (world.driverWait("//div[contains(text(),'Pay with card')]").isDisplayed()) {
+            world.driverWait("//input[@id=\"cardNumber\"]").sendKeys("4111111111111111");
+            world.driverWait("//input[@id=\"cardExpiry\"]").sendKeys("09/30");
+            world.driverWait("//input[@id=\"cardCvc\"]").sendKeys("123");
+            world.driverWait("//input[@id=\"billingName\"]").sendKeys("Md Habibullah Mia");
+            world.driverWait("//button[@type=\"submit\"]").click();
+
+        } else if (world.driver.findElement(By.xpath("//h4[text()='Payment Details']")).isDisplayed()) {
+            world.driverWait("//input[@name='credit card no']").sendKeys("4111111111111111");
+            world.driverWait("//input[@name='expiration date']").sendKeys("09/30");
+            world.driverWait("//input[@name='security code']").sendKeys("123");
+            world.driverWait("//input[@name='first name']").sendKeys("Md Habibullah");
+            world.driverWait("//input[@name='last name']").sendKeys("Mia");
+            world.driverWait("//input[@name='address']").sendKeys("44/27");
+            world.driverWait("//input[@name='city']").sendKeys("San Francisco");
+            world.driverWait("//input[@name='state']").sendKeys("California");
+            world.driverWait("//input[@name='zip']").sendKeys("94107");
+            world.driverWait("//button[contains(text(), ' Pay Now ')]").click();
+        }
+        String ClassOrderPlaced = world.driverWait("//div[@class='toast-message']").getText();
+        assert ClassOrderPlaced.contains("success");
+    }
+        @Then("I Select Product")
+        public void i_select_product() {
+            world.driverWait("//a[contains(text(), 'Packages')]//ancestor::ul//a[contains(text(), 'Product')]").click();
+            world.driverWait("(//td)[1]").click();
+            world.driverWait("//button[contains(text(), ' Add to Cart ')]").click();
+        }
+//        @Then("I pay for Product")
+//        public void i_pay_for_product() {
+//            ((JavascriptExecutor) world.driver).executeScript("window.scrollTo(document.body.scrollHeight, 0)");
+//
+//            world.driverWait("//button[contains(text(), 'Checkout')]").click();
+//            world.driverWait("//button[contains(text(), 'Provide Payment Info')]").click();
+//            world.driverWait("//button[contains(text(), ' Order Preview ')]").click();
+//            world.driverWait("//button[contains(text(), ' Make Payment ')]").click();
+//
+//
+//            if (world.driverWait("//div[contains(text(),'Pay with card')]").isDisplayed()) {
+//                world.driverWait("//input[@id=\"cardNumber\"]").sendKeys("4111111111111111");
+//                world.driverWait("//input[@id=\"cardExpiry\"]").sendKeys("09/30");
+//                world.driverWait("//input[@id=\"cardCvc\"]").sendKeys("123");
+//                world.driverWait("//input[@id=\"billingName\"]").sendKeys("Md Habibullah Mia");
+//                world.driverWait("//button[@type=\"submit\"]").click();
+//
+//            } else if (world.driver.findElement(By.xpath("//h4[text()='Payment Details']")).isDisplayed()) {
+//                world.driverWait("//input[@name='credit card no']").sendKeys("4111111111111111");
+//                world.driverWait("//input[@name='expiration date']").sendKeys("09/30");
+//                world.driverWait("//input[@name='security code']").sendKeys("123");
+//                world.driverWait("//input[@name='first name']").sendKeys("Md Habibullah");
+//                world.driverWait("//input[@name='last name']").sendKeys("Mia");
+//                world.driverWait("//input[@name='address']").sendKeys("44/27");
+//                world.driverWait("//input[@name='city']").sendKeys("San Francisco");
+//                world.driverWait("//input[@name='state']").sendKeys("California");
+//                world.driverWait("//input[@name='zip']").sendKeys("94107");
+//                world.driverWait("//button[contains(text(), ' Pay Now ')]").click();
+//            }
+//            String ProductOrderPlaced = world.driverWait("//div[@class='toast-message']").getText();
+//            assert ProductOrderPlaced.contains("success");
+//        }
+
+    @Then("I Select Rental")
+    public void i_select_rental() {
+        world.driverWait("(//a[contains(text(), 'Rental')])[2]").click();
+        WebElement select = world.driverWait("//select[@name='serial']");
+        Select selectPump = new Select(select);
+        selectPump.selectByIndex(1);
+
+        WebElement selectRT = world.driverWait("//select[@name='term']");
+        Select selectTerm = new Select(selectRT);
+        selectRT.click();
+        selectTerm.selectByIndex(1);
+        //selectTerm.selectByVisibleText("1 days for $20.00");
+
+        world.driverWait("//button[@type='submit']").click();
+        world.driverWait("//button[contains(text(), ' Add to Cart ')]").click();
+    }
+//    @Then("I pay for Rental")
+//    public void i_pay_for_rental() {
+//        ((JavascriptExecutor) world.driver).executeScript("window.scrollTo(document.body.scrollHeight, 0)");
+//
+//        world.driverWait("//button[contains(text(), 'Checkout')]").click();
+//        world.driverWait("//button[contains(text(), 'Provide Payment Info')]").click();
+//        world.driverWait("//button[contains(text(), ' Order Preview ')]").click();
+//        world.driverWait("//button[contains(text(), ' Make Payment ')]").click();
+//
+//
+//        if (world.driverWait("//div[contains(text(),'Pay with card')]").isDisplayed()) {
+//            world.driverWait("//input[@id=\"cardNumber\"]").sendKeys("4111111111111111");
+//            world.driverWait("//input[@id=\"cardExpiry\"]").sendKeys("09/30");
+//            world.driverWait("//input[@id=\"cardCvc\"]").sendKeys("123");
+//            world.driverWait("//input[@id=\"billingName\"]").sendKeys("Md Habibullah Mia");
+//            world.driverWait("//button[@type=\"submit\"]").click();
+//
+//        } else if (world.driver.findElement(By.xpath("//h4[text()='Payment Details']")).isDisplayed()) {
+//            world.driverWait("//input[@name='credit card no']").sendKeys("4111111111111111");
+//            world.driverWait("//input[@name='expiration date']").sendKeys("09/30");
+//            world.driverWait("//input[@name='security code']").sendKeys("123");
+//            world.driverWait("//input[@name='first name']").sendKeys("Md Habibullah");
+//            world.driverWait("//input[@name='last name']").sendKeys("Mia");
+//            world.driverWait("//input[@name='address']").sendKeys("44/27");
+//            world.driverWait("//input[@name='city']").sendKeys("San Francisco");
+//            world.driverWait("//input[@name='state']").sendKeys("California");
+//            world.driverWait("//input[@name='zip']").sendKeys("94107");
+//            world.driverWait("//button[contains(text(), ' Pay Now ')]").click();
+//        }
+//        String ProductOrderPlaced = world.driverWait("//div[@class='toast-message']").getText();
+//        assert ProductOrderPlaced.contains("success");
+//    }
 
         //world.driverWait("").click();
-    }
+
 }
+
